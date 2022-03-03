dda51217e7b63489b720700200cb57bca268ff9a
Nicole Toole <nicoletoole@nicoles-mbp.wifi.gsu.edu>
2022-03-02 23:54:44 -0500

    MORE EDITS

--- app/src/main/java/com/example/newproject/MainActivity.java
+++ app/src/main/java/com/example/newproject/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.newproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListButton();
+        initSettingsButton();
+        initToggleButton();
+
     }
+
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //reference created for current activity and which activity to start)
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                //intent flag set to alert the operating system to not make multiple copies of same activity
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initToggleButton() {
+        final ToggleButton editToggle = findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                setForEditing(editToggle.isChecked());
+
+
+            }
+        });
+    }
+
+    private void setForEditing(boolean enabled) {
+
+       EditText editSubject = findViewById(R.id.subField);
+       EditText editNote = findViewById(R.id.noteField);
+       RadioGroup radioGroup = findViewById(R.id.radPriority);
+       Button buttonSave = findViewById(R.id.butttonSave);
+
+
+       buttonSave.setEnabled(enabled);
+       editNote.setEnabled(enabled);
+       radioGroup.setEnabled(enabled);
+
+        if (enabled) {
+            editSubject.requestFocus();
+        }
+
+    }
+
+
+
+
 }
